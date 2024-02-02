f9be7bc0c0f16d96cb3c442a341a42b4985ca00a
Guilherme Scherer <126033435+Gui162@users.noreply.github.com>
2024-02-02 11:22:00 -0300

    Delete Emprestimos de livros em ASP/EmprestimoLivros directory
    farei um repositório especifico para esse projeto

--- Emprestimos de livros em ASP/EmprestimoLivros/EmprestimoLivros/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $('#Emprestimos').DataTable();
-
-    setTimeout(function () {
-        $(".alert").fadeOut("slow", function () {
-            $(this).alert('close');
-        });
-    }, 5000);
-});
